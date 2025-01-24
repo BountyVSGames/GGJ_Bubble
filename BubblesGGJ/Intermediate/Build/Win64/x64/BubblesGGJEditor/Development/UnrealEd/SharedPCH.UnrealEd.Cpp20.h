7900b6918f5aab7cc16fba5c36ba0fc110fe8104
BountyVSGames <191056@buas.nl>
2025-01-24 21:25:00 +0100

    Removed build files and reworked gitignore file

--- BubblesGGJ/Intermediate/Build/Win64/x64/BubblesGGJEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/boude/Documents/School/Prerfoce-Swarm-3/Boudewijn191056/BubblesGGJ/Intermediate/Build/Win64/x64/BubblesGGJEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
